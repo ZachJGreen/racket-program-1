43207135dd888b99516327cc029e5e884fb2533e
ZachJGreen <green.zach.j@gmail.com>
2026-09-10 21:56:54 -0500

    refactor: improve test output formatting for clarity

--- evaluator.rkt
+++ evaluator.rkt
@@ -20,24 +20,25 @@
     ]
 
     [`(binary-op ,op ,left ,right)
+      (printf "Operator: ~a\n" op)
       (eval-expr left env)
       (eval-expr right env)
     ]
     [else (error "Invalid AST node structure")]))
 
-(displayln "\nLiteral")
+(displayln "\nLiteral Test")
 (eval-expr '(lit 157) #hash())
 
-(displayln "\nVariable, no hash")
+(displayln "\nVariable, no hash Test")
 (eval-expr '(var "x") #hash())
 
-(displayln "\nVariable, with hash")
+(displayln "\nVariable, with hash Test")
 (eval-expr '(var "x") #hash(("x" . 27)))
 
-(displayln "\nDisplay Subexpression")
+(displayln "\nDisplay Subexpression Test")
 (eval-expr '(not (var x)) #hash())
 
-(displayln "\nBinary Operation")
+(displayln "\nBinary Operation Test")
 (eval-expr '(binary-op "+" (lit 2) (binary-op "*" (var "x") (lit 4))) #hash(("x" . 3)))
 
 
