a4544650efed3f8eb0726a51bfa01b30ef9235bd
ZachJGreen <green.zach.j@gmail.com>
2026-09-15 08:15:21 -0500

    cleanup

--- evaluator.rkt
+++ evaluator.rkt
@@ -13,12 +13,9 @@
 
     [`(not ,subex) 
       (eval-expr subex env)
-
     ]
 
     [`(binary-op ,op ,left ,right)
-      (printf "\nBINARY OP\n")
-      (printf "Operator: ~a\n" op)
       (eval-expr left env)
       (eval-expr right env)
       (equate op left right env)
@@ -26,8 +23,6 @@
     [else (error "Invalid AST node structure")]))
 
 (define (equate op left right env)
-  (printf "\n   EQUATE ~a\n" op)
-  (printf "\nLeft: ~a\nRight: ~a\n" left right)
 
   ; if left evaluates to maybe or right evaluates to maybe
   (if (or (equal? (eval-expr left env) 'maybe) (equal? (eval-expr right env) 'maybe))
@@ -55,13 +50,13 @@
 
 
 ;(displayln "\nLiteral Test")
-(eval-expr '(lit 157) #hash())
+;(eval-expr '(lit 157) #hash())
 
 ;(displayln "\nVariable, no hash Test")
-(eval-expr '(var "x") #hash())
+;(eval-expr '(var "x") #hash())
 
 ;(displayln "\nVariable, with hash Test")
-(eval-expr '(var "x") #hash(("x" . 27)))
+;(eval-expr '(var "x") #hash(("x" . 27)))
 
 ;(displayln "\nDisplay Subexpression Test")
 ;(eval-expr '(not (var x)) #hash(("x" . "hello")))
