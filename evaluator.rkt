164f8002d644a1d1efae5ad7f29e3dc675147f50
Zach Green <112576058+ZachJGreen@users.noreply.github.com>
2026-09-14 23:21:18 -0500

    Figured out the Binary Operation in the format that the feeble minded OOP Programmer could not comprehend

--- evaluator.rkt
+++ evaluator.rkt
@@ -4,41 +4,85 @@
   (match expr
 
     [`(lit ,val) 
-      (printf "'(lit ~a)\n" val)
+      val
     ]
 
     [`(var ,id) 
-      (if (hash-empty? env)
-        (begin (printf "'(var ~a)\n" id))
-        (begin (printf "'(var ~a) with a value of ~a\n" id (hash-ref env id)))
-      )
+      (eval-expr (list 'lit (hash-ref env id 'maybe)) env)
     ]
 
     [`(not ,subex) 
-      (displayln subex)
+      (eval-expr subex env)
 
     ]
 
     [`(binary-op ,op ,left ,right)
+      (printf "\nBINARY OP\n")
       (printf "Operator: ~a\n" op)
       (eval-expr left env)
       (eval-expr right env)
+      (equate op left right env)
     ]
     [else (error "Invalid AST node structure")]))
 
-(displayln "\nLiteral Test")
+(define (equate op left right env)
+  (printf "\n   EQUATE ~a\n" op)
+  (printf "\nLeft: ~a\nRight: ~a\n" left right)
+
+  ; if left evaluates to maybe or right evaluates to maybe
+  (if (or (equal? (eval-expr left env) 'maybe) (equal? (eval-expr right env) 'maybe))
+    ; True Case:
+    (begin 
+      'maybe
+    )
+
+    ; False Case
+    (begin 
+      (match op
+        ["+"
+          ( +
+            (eval-expr left env)
+            (eval-expr right env)
+          )
+        ]
+        ["-"
+          ( -
+            (eval-expr left env)
+            (eval-expr right env)
+          )
+        ]
+        ["*"
+          ( *
+            (eval-expr left env)
+            (eval-expr right env)
+          )
+        ]
+        ["/"
+          ( /
+            (eval-expr left env)
+            (eval-expr right env)
+          )
+        ]
+      )
+    )
+  )
+)
+
+
+;(displayln "\nLiteral Test")
 (eval-expr '(lit 157) #hash())
 
-(displayln "\nVariable, no hash Test")
+;(displayln "\nVariable, no hash Test")
 (eval-expr '(var "x") #hash())
 
-(displayln "\nVariable, with hash Test")
+;(displayln "\nVariable, with hash Test")
 (eval-expr '(var "x") #hash(("x" . 27)))
 
-(displayln "\nDisplay Subexpression Test")
-(eval-expr '(not (var x)) #hash())
+;(displayln "\nDisplay Subexpression Test")
+;(eval-expr '(not (var x)) #hash(("x" . "hello")))
 
-(displayln "\nBinary Operation Test")
+;(displayln "\nBinary Operation Test")
 (eval-expr '(binary-op "+" (lit 2) (binary-op "*" (var "x") (lit 4))) #hash(("x" . 3)))
 
-
+;(displayln "\nBasic Operation Test\n")
+;(eval-expr '(binary-op "*" (var "y") (lit 4)) #hash(("x" . 5)))
